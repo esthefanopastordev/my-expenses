5b828ccd1c844035a119542e57757f3697fa6c6a
Esthefano Pastor <epastopa@emeal.nttdata.com>
2022-11-24 07:18:13 -0500

    Initial Commit

---
+++ my-expenses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my-expenses
+//
+//  Created by Esthefano Oniel Pastor Palacios on 24/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
