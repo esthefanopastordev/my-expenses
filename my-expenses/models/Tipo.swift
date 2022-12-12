a4e24a3bbc6dd57361211e6af2dfdef4088f9f6d
Esthefano Pastor <epastopa@emeal.nttdata.com>
2022-12-12 08:20:33 -0500

    Render categories on a table

---
+++ my-expenses/models/Tipo.swift
@@ -0,0 +1,13 @@
+//
+//  Tipo.swift
+//  my-expenses
+//
+//  Created by Esthefano Oniel Pastor Palacios on 9/12/22.
+//
+
+import Foundation
+
+struct Tipo {
+    let id: String
+    let name: String
+}
